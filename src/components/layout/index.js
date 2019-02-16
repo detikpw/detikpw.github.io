79326e15e2d156bf2319bf8c42c444cd92a1680b
Detik <the2ndflamingheart@gmail.com>
2019-02-16 17:49:30 +0700

    Create layout component

---
+++ src/components/layout/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+import { createGlobalStyle } from 'styled-components'
+
+import Header from './Header';
+import Sidebar from './Sidebar';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`
+
+export default class extends Component {
+    state = {
+        isSidebarOpen: false
+    }
+
+    openSidebar = () => this.setState({ isSidebarOpen: true })
+    closeSidebar = () => this.setState({ isSidebarOpen: false })
+    render() {
+        const { children } = this.props;
+        const { isSidebarOpen } = this.state;
+        return (
+            <div>
+                <Helmet>
+                    <link href="https://fonts.googleapis.com/css?family=Exo" rel="stylesheet"/>
+                </Helmet>
+                <GlobalStyle />
+                <Header
+                    onOpenSidebar={this.openSidebar}
+                    onCloseSidebar={this.closeSidebar}
+                />
+                <Sidebar
+                    onCloseSidebar={this.closeSidebar}
+                    isSidebarOpen={isSidebarOpen}
+                />
+                {children}
+            </div>
+        );
+    }
+}
+
