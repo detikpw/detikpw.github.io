7118b375c98245510eb7ff3669e5ea49224708b9
Detik <the2ndflamingheart@gmail.com>
2019-02-19 08:37:02 +0700

    Add some new field in gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
 module.exports = {
-    plugins: ['gatsby-plugin-flow', 'gatsby-plugin-styled-components', 'gatsby-plugin-react-helmet'],
+    siteMetadata: {
+      title: `Catatan Bambang`,
+    },
+    plugins: ['gatsby-plugin-flow', 'gatsby-plugin-styled-components', 'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "markdown-pages",
+      },
+    },
+  ],
 }
