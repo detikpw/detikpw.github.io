d756211cbb12afd47fe660a43e9fc2489e118aaa
Detik <the2ndflamingheart@gmail.com>
2019-02-19 08:37:48 +0700

    Use query data instead of hardcode for site title

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,39 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Box, Button,  Flex, Text, Card } from 'rebass';
+import { StaticQuery, graphql } from 'gatsby';
 import Menu from '../icons/Menu';
 
-export default class Header extends Component {
-
-  render() {
-    const { onOpenSidebar, onCloseSidebar } = this.props;
-    return (
-      <Card
-        boxShadow="0 1px 0 rgba(0,0,0,0.15)"
-      >
-        <Flex
-          px={3}
-          alignItems="center"
-          justifyContent="start"
-          flexDirection="row"
-          css={{
-              height: '40px'
-          }}
+const header = ({ onOpenSidebar }) => data => (
+  <Card
+    boxShadow="0 1px 0 rgba(0,0,0,0.15)"
+  >
+    <Flex
+      px={3}
+      alignItems="center"
+      justifyContent="start"
+      flexDirection="row"
+      css={{
+          height: '40px'
+      }}
+    >
+      <Box alignSelf="center" width={1/6}>
+        <Button px={0} bg="white" onClick={onOpenSidebar}>
+          <Menu />
+        </Button>
+      </Box>
+      <Box alignSelf="center" width={4/6}>
+        <Text
+          fontFamily="Exo"
+          fontWeight="bold"
         >
-          <Box alignSelf="center" width={1/6}>
-            <Button px={0} bg="white" onClick={onOpenSidebar}>
-              <Menu />
-            </Button>
-          </Box>
-          <Box alignSelf="center" width={4/6}>
-            <Text
-              fontFamily="Exo"
-              fontWeight="bold"
-            >
-              Catatan Detik
-            </Text>
-          </Box>
-        </Flex>
-      </Card>
-    );
-  }
-};
+          {data.site.siteMetadata.title}
+        </Text>
+      </Box>
+    </Flex>
+  </Card>
+);
+
+const Header = ({ onOpenSidebar }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }`
+    }
+    render={header({ onOpenSidebar })}
+  />
+);
+
+export default Header;
