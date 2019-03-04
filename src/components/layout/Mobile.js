801b7de2a0fea04780291075ed1bb296c27c5f89
Detik <the2ndflamingheart@gmail.com>
2019-03-04 17:20:08 +0700

    Create mobile layout to render mobile screen only

---
+++ src/components/layout/Mobile.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Flex, Box } from 'rebass';
+import Header from './Header';
+import Footer from './Footer';
+import Sidebar from '../sidebar';
+import MobileScreen from '../devices/Mobile';
+
+export default class Mobile extends Component {
+  state = {
+    isSidebarOpen: false
+  }
+
+  openSidebar = () => this.setState({ isSidebarOpen: true })
+  closeSidebar = () => this.setState({ isSidebarOpen: false })
+
+  render() {
+    const { children } = this.props
+    const { isSidebarOpen } = this.state;
+    return (
+      <MobileScreen>
+        <Sidebar
+          onCloseSidebar={this.closeSidebar}
+          isSidebarOpen={isSidebarOpen}
+        />
+        <Flex
+          flexDirection="column"
+          css={{
+            height: '100%',
+          }}
+        >
+          <Header
+            onOpenSidebar={this.openSidebar}
+            onCloseSidebar={this.closeSidebar}
+          />
+          <Box
+            pt="40px"
+            css={{
+              flexGrow: 1,
+              overflowY: 'auto'
+            }}
+          >
+            {children}
+          </Box>
+          <Footer />
+        </Flex>
+      </MobileScreen>
+    );
+  }
+}
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import { createGlobalStyle } from 'styled-components'
 import { Box, Flex } from 'rebass';
-
-import Header from './Header';
-import Footer from './Footer';
-import Sidebar from '../sidebar';
 import { overflow, position } from 'styled-system';
+import MobileLayout from './Mobile';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -35,31 +32,9 @@ export default class extends Component {
           <link href="https://fonts.googleapis.com/css?family=Exo" rel="stylesheet"/>
         </Helmet>
         <GlobalStyle />
-        <Sidebar
-          onCloseSidebar={this.closeSidebar}
-          isSidebarOpen={isSidebarOpen}
-        />
-        <Flex
-          flexDirection="column"
-          css={{
-            height: '100%',
-          }}
-        >
-          <Header
-            onOpenSidebar={this.openSidebar}
-            onCloseSidebar={this.closeSidebar}
-          />
-          <Box
-            pt="40px"
-            css={{
-              flexGrow: 1,
-              overflowY: 'auto'
-            }}
-          >
-            {children}
-          </Box>
-          <Footer />
-        </Flex>
+        <MobileLayout>
+          {children}
+        </MobileLayout>
       </Box>
     );
   }
