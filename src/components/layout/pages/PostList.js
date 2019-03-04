daae8d36a95ff4213f7866a50d4ab612d6fb5352
Detik <the2ndflamingheart@gmail.com>
2019-03-04 21:31:43 +0700

    Separate render posts in home page to anohter component

---
+++ src/components/layout/pages/PostList.js
@@ -0,0 +1,74 @@
+import type { Node } from 'react';
+
+import React from 'react';
+import { graphql } from "gatsby";
+import { map, prop } from 'ramda';
+import { Flex } from 'rebass';
+import Mobile from '../../devices/Mobile';
+import Layout from '../../layout';
+import HeaderSection from '../../layout/pages/HeaderSection';
+import Body from '../../layout/pages/Body';
+import Articles from '../../layout/pages/Articles';
+import ArticleSummary from '../../layout/pages/ArticleSummary';
+import PageHeader from '../../typography/PageHeader';
+import Topic from '../../typography/Topic';
+import ArticleTitle from '../../typography/ArticleTitle';
+import ArticleBody from '../../typography/ArticleBody';
+import Caption from '../../typography/Caption';
+import Link from '../../link';
+import Image from '../../images/Image';
+import MobileScreen from '../../devices/Mobile';
+
+const renderPosts = ({ frontmatter, id, excerpt, fields }) => {
+  const { topic, image, title, date } = frontmatter;
+  const { path } = fields;
+  return (
+    <Articles key={id}>
+      {topic && <Topic>{topic}</Topic>}
+      {image.src && (
+        <Image
+          {...image}
+          url={path}
+        />
+      )}
+      <Link
+        withTextDecoration={false}
+        to={path}
+      >
+        <ArticleSummary>
+          <ArticleTitle>{title}</ArticleTitle>
+          <ArticleBody><div dangerouslySetInnerHTML={{ __html: excerpt }}/></ArticleBody>
+        </ArticleSummary>
+      </Link>
+      <Caption>
+        {date}
+      </Caption>
+      <hr />
+    </Articles>
+  );
+}
+export default ({ posts, pageTitle }): Node => {
+  return (
+    <Layout>
+      <Flex flexDirection="column">
+        <MobileScreen>
+          <Image
+            src="https://images.unsplash.com/photo-1421882402971-b18cd1741ac6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1488&q=80"
+            url="https://unsplash.com/photos/gm3bxHin8VA"
+            caption="Photo By Daria Nepriakhina"
+            captionUrl="https://unsplash.com/@epicantus"
+            captionPx={3}
+          />
+        </MobileScreen>
+        <Body>
+          <HeaderSection>
+            <PageHeader>
+              {pageTitle}
+            </PageHeader>
+          </HeaderSection>
+          {posts.map(renderPosts)}
+        </Body>
+      </Flex>
+    </Layout>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,49 +6,8 @@ import React from 'react';
 import { graphql } from "gatsby";
 import { map, prop } from 'ramda';
 import { Flex } from 'rebass';
-import Mobile from '../components/devices/Mobile';
-import Layout from '../components/layout';
-import HeaderSection from '../components/layout/pages/HeaderSection';
-import Body from '../components/layout/pages/Body';
-import Articles from '../components/layout/pages/Articles';
-import ArticleSummary from '../components/layout/pages/ArticleSummary';
-import PageHeader from '../components/typography/PageHeader';
-import Topic from '../components/typography/Topic';
-import ArticleTitle from '../components/typography/ArticleTitle';
-import ArticleBody from '../components/typography/ArticleBody';
-import Caption from '../components/typography/Caption';
-import Link from '../components/link';
-import Image from '../components/images/Image';
-import MobileScreen from '../components/devices/Mobile';
+import PostList from '../components/layout/pages/PostList';
 
-const renderPosts = ({ frontmatter, id, excerpt, fields }) => {
-  const { topic, image, title, date } = frontmatter;
-  const { path } = fields;
-  return (
-    <Articles key={id}>
-      {topic && <Topic>{topic}</Topic>}
-      {image.src && (
-        <Image
-          {...image}
-          url={path}
-        />
-      )}
-      <Link
-        withTextDecoration={false}
-        to={path}
-      >
-        <ArticleSummary>
-          <ArticleTitle>{title}</ArticleTitle>
-          <ArticleBody><div dangerouslySetInnerHTML={{ __html: excerpt }}/></ArticleBody>
-        </ArticleSummary>
-      </Link>
-      <Caption>
-        {date}
-      </Caption>
-      <hr />
-    </Articles>
-  );
-}
 export default ({
     data: {
       allMarkdownRemark: { edges },
@@ -56,27 +15,7 @@ export default ({
 }): Node => {
   const posts = map(prop('node'), edges)
   return (
-    <Layout>
-      <Flex flexDirection="column">
-        <MobileScreen>
-          <Image
-            src="https://images.unsplash.com/photo-1421882402971-b18cd1741ac6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1488&q=80"
-            url="https://unsplash.com/photos/gm3bxHin8VA"
-            caption="Photo By Daria Nepriakhina"
-            captionUrl="https://unsplash.com/@epicantus"
-            captionPx={3}
-          />
-        </MobileScreen>
-        <Body>
-          <HeaderSection>
-            <PageHeader>
-              Terbaru
-            </PageHeader>
-          </HeaderSection>
-          {posts.map(renderPosts)}
-        </Body>
-      </Flex>
-    </Layout>
+    <PostList posts={posts} pageTitle="Terbaru" />
   )
 }
 
