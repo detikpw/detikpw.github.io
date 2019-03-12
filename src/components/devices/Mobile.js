9847bc2e0858769bf619b2d438428007bf535d60
detikpw <the2ndflamingheart@gmail.com>
2019-03-13 05:30:30 +0700

    Updates

--- src/components/devices/Mobile.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-import React from 'react';
-import Responsive from 'react-responsive';
-
-type Props = { [string]: * }
-
-export default (props: Props) => <Responsive {...props} maxWidth="64em" />;
