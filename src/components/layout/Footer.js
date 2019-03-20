ca699861aebfd889910fc6dc729807a51555be86
detikpw <the2ndflamingheart@gmail.com>
2019-03-21 03:36:26 +0700

    Remove copyright

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => (
           fontSize=".625rem"
           lineHeight={1.2}
         >
-          © 2019 Detik P Warjaya. Powered by <Link to="https://www.gatsbyjs.org/">Gatsby</Link>
+          Powered by <Link to="https://www.gatsbyjs.org/">Gatsby</Link>
           &nbsp; inspired by jekyll theme <Link to="https://github.com/mmistakes/so-simple-theme">
           So Simple Theme </Link> and <Link to="https://www.vice.com">vice.com</Link>
         </Text>
